8d7973c9c81b5a46b79ab88b945abee806b5aaa7
ROG STRIX G <knazara@gmail.com>
2022-09-18 23:53:04 +0700

    Update Promise

--- promise.js
+++ promise.js
@@ -1,8 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ekspresipenonton) => {
+  try {
+    const x = await promiseTheaterIXX();
+    const y = await promiseTheaterVGC();
+    const semuaFilm = [...x, ...y];
 
+    const arrEkspresi = semuaFilm.filter((cd) => cd.hasil === ekspresipenonton);
+    let bykEkspresi = `${ekspresipenonton} : ${arrEkspresi.length}`;
+
+    return bykEkspresi;
+  } catch(err){
+    console.log(err);
+  }
+
+}
 module.exports = {
   promiseOutput,
 };
